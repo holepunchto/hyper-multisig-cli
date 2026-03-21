f97cff5516e8eccb8268a7f03f1c60f3029e67ba
Marco <maidh91@gmail.com>
2026-03-21 19:45:43 +1100

    hypercore-sign-4 (#13)
    * hypercore-sign-4
    
    * hypercore-sign-4
    
    * hypercore-sign-4
    
    * bump hyper-multisig
    
    * Multisig.getCoreKey
    
    * hypercore-sign-4

--- test.js
+++ test.js
@@ -9,12 +9,11 @@ const process = require('process')
 const crypto = require('hypercore-crypto')
 const idEnc = require('hypercore-id-encoding')
 const createTestnet = require('hyperdht/testnet')
-const SignMessages = require('hypercore-sign/lib/messages')
-const SignSecure = require('hypercore-sign/lib/secure')
+const CoreSign = require('hypercore-sign')
 const SignRequest = require('hypercore-signing-request')
 const Hyperdrive = require('hyperdrive')
 const Hyperswarm = require('hyperswarm')
-const MultisigUtil = require('hyper-multisig/lib/util')
+const Multisig = require('hyper-multisig')
 const NewlineDecoder = require('newline-decoder')
 const sodium = require('sodium-native')
 const { isBare } = require('which-runtime')
@@ -77,11 +76,7 @@ test('link', async (t) => {
 
   await tLinkCore
 
-  t.is(
-    coreKey,
-    idEnc.normalize(MultisigUtil.getCoreKey(publicKeys, namespace)),
-    'core key is correct'
-  )
+  t.is(coreKey, idEnc.normalize(Multisig.getCoreKey(publicKeys, namespace)), 'core key is correct')
 })
 
 test('core request and sign CLI flow', async (t) => {
@@ -954,7 +949,7 @@ function setupMultisig(namespace = 'holepunchto/my-test', numSigners = 5) {
     const password = sodium.sodium_malloc(8)
     sodium.randombytes_buf_deterministic(password, seed)
 
-    const keys = SignSecure.generateKeys(password)
+    const keys = CoreSign.generateKeys(password)
     signers.push({ ...keys, seed })
   }
   const publicKeys = signers.map((signer) => idEnc.normalize(signer.publicKey))
@@ -964,7 +959,7 @@ function setupMultisig(namespace = 'holepunchto/my-test', numSigners = 5) {
 
 function signResponse(request, signer) {
   const { clonedSigner, decodedReq, signatures } = sign(request, signer)
-  const res = cenc.encode(SignMessages.Response, {
+  const res = cenc.encode(CoreSign.Response, {
     version: decodedReq.version,
     requestHash: crypto.hash(request),
     publicKey: clonedSigner.publicKey,
@@ -981,12 +976,13 @@ function sign(request, signer) {
   }, {})
 
   const decodedReq = SignRequest.decode(request)
-  const signables = SignRequest.signable(clonedSigner.publicKey, decodedReq)
 
   const password = sodium.sodium_malloc(8)
   sodium.randombytes_buf_deterministic(password, clonedSigner.seed)
 
-  const signatures = SignSecure.sign(signables, clonedSigner.secretKey, password)
+  const response = CoreSign.sign(request, clonedSigner.secretKey, password)
+  const { signatures } = cenc.decode(CoreSign.Response, response)
+
   return { clonedSigner, decodedReq, signatures }
 }
 
